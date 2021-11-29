63904ebab3ef62c30ebb53506db2fbdec90168c0
Mark Otterson <otterson.2@gmail.com>
2021-11-29 01:18:31 -0500

    initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { onMount } from 'svelte';
+
+	let teams = [];
+
+	onMount(async () => {
+		// @ts-ignore
+		const res = await fetch(import.meta.env['VITE_TEAMS_API_URL']);
+		teams = await res.json();
+	});
+</script>
+
+<div class="teams">
+    <div>
+        {#each teams as team}
+            <div>
+                {team.city}
+            </div>
+        {/each}
+    </div>
+</div>
+
+<style>
+
+</style>
