95fe46a261ee449bcc1308611e05d67046f702f0
Mark Otterson <otterson.2@gmail.com>
2021-11-29 01:42:06 -0500

    add some dummy css

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -13,15 +13,27 @@
 </script>
 
 <div class="teams">
-    <div>
-        {#each teams as team}
-            <div style="color:{team.primary};">
-                {team.city} {team.nickname}
-            </div>
-        {/each}
-    </div>
+    {#each teams as team}
+        <div class="team" style="background-color:{team.primary};">
+            {team.city} {team.nickname}
+        </div>
+    {/each}
 </div>
 
 <style>
-
+.teams{
+    display:flex;
+    flex-direction: column;
+}
+.team{
+    padding:10px;
+    border:1px rgb(213, 213, 213) solid;
+    border-radius:999px;
+    width:max-content;
+    margin-bottom:5px;
+    margin-right:5px;
+    font-family:sans-serif;
+    font-weight:bold;
+    color:white;
+}
 </style>
